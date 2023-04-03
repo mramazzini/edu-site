766d6b91ffb44fda8ca3d0e1a0ee82c272b4b391
mramazzini <mramazzini123@gmail.com>
2023-04-03 09:30:45 -0500

    added main page header

---
+++ client/src/components/home/HomeBody.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const HomeBody = () => {
+  return (
+    <div className="home-body">
+      <div className="home-body-container">
+        <div className="home-body-left">
+          <header className="home-body-left-header">
+            <span className="header-head">Learn to Code?</span>
+
+            <span className="header-body">Let me teach you</span>
+            <div className="get-started-nav">
+              <a href="/">
+                <button>Contact</button>
+              </a>
+              <a href="/">
+                <button>Learn more</button>
+              </a>
+            </div>
+          </header>
+        </div>
+        <div className="home-body-seperator-right"></div>
+        <div className="home-body-seperator-line"></div>
+        <div className="home-body-seperator-left"></div>
+        <div className="home-body-right">
+          <header className="home-body-right-header">
+            <h1>Home Body Right</h1>
+          </header>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeBody;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState, useRef } from "react";
 import Navbar from "../components/Navbar";
+import HomeBody from "../components/home/HomeBody";
 import "../styles/Home.css";
 function Home() {
   return (
     <div className="home-page">
       <Navbar />
+      <HomeBody />
     </div>
   );
 }
